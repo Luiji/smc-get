a00149af5081fd7ca543f6dc4697de78f2ef5d17
Quintus <sutniuq@gmx.net>
2011-02-11 21:30:35 +0100

    Added copyright notice.

--- lib/smc_get/cui.rb
+++ lib/smc_get/cui.rb
@@ -1,5 +1,22 @@
-#!/usr/bin/env ruby
 #Encoding: UTF-8
+################################################################################
+# This file is part of smc-get.
+# Copyright (C) 2010-2011 Entertaining Software, Inc.
+# Copyright (C) 2011 Marvin Gülker
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+################################################################################
 
 module SmcGet
   
